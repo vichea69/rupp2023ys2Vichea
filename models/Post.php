024dacd8b597ea973a08301da0fc9502b3ac5eb8
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-03-14 19:52:57 +0700

    [TASK] Define global post to store centralize post data

--- models/Post.php
+++ models/Post.php
@@ -10,5 +10,12 @@ class Post {
     ) {}
 }
 
+if (!isset($GLOBALS['posts'])) {
+    $GLOBALS['posts'] = [
+        new Post(1, 'Post 1', 'Content 1', 'https://fastly.picsum.photos/id/123/300/200.jpg?hmac=kXYDwT491zyy8kdoIlZfMs-IUzLA5VTv6DKX2dq5MO0'),
+        new Post(2, 'Post 2', 'Content 2', 'https://fastly.picsum.photos/id/123/300/200.jpg?hmac=kXYDwT491zyy8kdoIlZfMs-IUzLA5VTv6DKX2dq5MO0'),
+        new Post(3, 'Post 3', 'Content 3', 'https://fastly.picsum.photos/id/123/300/200.jpg?hmac=kXYDwT491zyy8kdoIlZfMs-IUzLA5VTv6DKX2dq5MO0')
+    ];
+}
 
 ?>
