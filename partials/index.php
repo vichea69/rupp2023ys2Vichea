126e93d8e838d3146926c0d7ceda5857b1b2083b
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-03-14 18:45:04 +0700

    [TASK] Kickstart clean blog project

---
+++ partials/index.php
@@ -0,0 +1,13 @@
+<?php
+    function renderMenu() {
+        echo '
+            <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.php">Sample Post</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact</a></li>
+            </ul>
+        ';
+    }
+
+?>
